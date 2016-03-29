fb04ec273684e6e43a26eab20c9ae25440cdea98
_图奇 <189812524@qq.com>
2016-03-29 22:27:12 +0800

    update

--- libcurl/libcurl.cpp
+++ libcurl/libcurl.cpp
@@ -3,10 +3,32 @@
 
 #include "stdafx.h"
 #include<iostream>
+#include<curl\curl.h>
+
+#pragma comment(lib,"libcurl.lib")
+
+void Test()
+{
+	CURL* curl = curl_easy_init();
+
+	if (nullptr == curl)
+	{
+		std::cout << "curl_easy_init error!\n";
+		return;
+	}
+	curl_easy_setopt(curl, CURLOPT_URL, "www.baidu.com");
+	curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+
+	curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	curl_global_init(CURL_GLOBAL_ALL);
+	Test();
 
+	curl_global_cleanup();
 	system("pause");
 	return 0;
 }
